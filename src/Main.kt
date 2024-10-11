e2ceac289439b0ddd23e901ee474fd98cb878e35
Саша <sanya.burayev.03@mail.ru>
2024-10-11 23:18:58 +0300

    Второй коммит

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,87 @@
-val max_age = 100//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.text.Position
+
+val max_age = 100 //Константа
+
+//Функции
+
+fun print_hello(){
+    println("Hello")
+}
+
+fun sendMessage(message:String){
+    println(message)
+}
+
+fun dispalyUser(name:String , age:Int){
+    println("Name: $name \t Age : $age")
+}
+
+fun dispalyUserDefault(name: String , age: Int = 18 , position: String = "unemplyed"){
+    println("Name : $name , Age : $age , Position : $position")
+}
+
+fun operationsArray(nums : Array<Int>){
+    nums[0] = nums[0] * 5
+    println("Значение в функции double: ${nums[0]}")
+}
+
+fun printStrings(vararg strings: String){
+    for(str in strings){
+        println(str)
+    }
+}
+
+fun sum(vararg nums:Int){
+    var result = 0
+    for (i in nums){
+        result += i
+    }
+    println("Сумма чисел равна $result")
+}
+
+fun printUserGroup(count:Int , vararg name: String){
+    println("Count : $count")
+    for(user in name){
+        println(user)
+    }
+}
+
+fun changeNumber(vararg numbers: Int , koef: Int){
+    for(num in numbers){
+        println(num* koef)
+    }
+}
+
+fun sums(x:Int, y:Int): Int {
+    return x + y
+}
+
+fun hi() : Unit{
+    println("Hi")
+}
+
+fun checkAge(age:Int){
+    if(age < 0 || age > 100){
+        println("Invalid age")
+        return
+    }
+    println("Age is valid")
+}
+
+fun square(x:Int) = x*x
+
+fun compareAge(age1 : Int , age2 : Int){
+    fun ageIsValid(age : Int): Boolean{
+        return age > 0 && age < 100
+    }
+    if(!ageIsValid(age1) || !ageIsValid(age2)){
+        println("Invalid age")
+    }
+    else{
+        println("Age is valid")
+    }
+}
+
 fun main() {
 
     // начало работы и знакомство с переменными
@@ -95,90 +177,124 @@ fun main() {
 //    println("Ваш адрес: $address")
 
     //условия if else
-    val ab = 10
-    if(ab == 10) {
-        println("ab = 10")
-    }
-    else{
-        println("ab != 10")
-    }
+//    val ab = 10
+//    if(ab == 10) {
+//        println("ab = 10")
+//    }
+//    else{
+//        println("ab != 10")
+//    }
+//
+//    val bg = 2.6
+//    if(bg == 2.6){
+//        println(true)
+//    }
+//    else if(bg == 2.5)
+//        println("default")
+//    else{
+//        println(false)
+//    }
+//
+//    //конструкция when
+//    val isEnabled = true
+//    when(isEnabled){
+//        false -> println("isEnabledOFF")
+//        true -> println("isEnabledON")
+//    }
+//
+//    val but = 30
+//    when(but){
+//        10 -> println("but = 10")
+//        20 -> println("but = 20")
+//        else -> println("неопределённое значение")
+//    }
+//
+//    when(but){
+//        in 10..19 -> println("Значение в диапазоне от 10 до 19")
+//        in 20 .. 29 -> println("Значение в диапазоне от 20 до 29")
+//        !in 10..20 -> println("Значение вне диапазона 10..19")
+//        else -> println("Неопределённое значение")
+//    }
+//
+//    when(but){
+//        10,20 -> println("Значение или 10 или 20")
+//        else -> println("Неопределённое значение")
+//    }
+//
+//    var ag = 10
+//    when(ag){
+//        10 -> {
+//            println("ag = 10")
+//            ag *= 2
+//        }
+//        20 -> {
+//            println("ag = 20")
+//            ag *= 5
+//        }
+//        else -> { println("неопределенное значение")}
+//    }
+//    println(ag)
+//
+//
+//    val a = 10
+//    val b = 5
+//    val c = 3
+//    when(a){
+//        b - c -> println("a = b - c")
+//        b + 5 -> println("a = b + 5")
+//        else -> println("неопределенное значение")
+//    }
+//
+//    val cb = 14
+//    val cc = 6
+//    when{
+//        (cb > 13) -> println("cb больше 13")
+//        (cc > 13) -> println("cc больше 13")
+//        else -> println("cb и cc меньше или равны 13")
+//    }
+//
+//    val day = 2
+//    var dayOfWeek = when(day){
+//        1 -> "Monday"
+//        2 -> "Tuesday"
+//        3 -> "Wednesday"
+//        4 -> "Thursday"
+//        else -> ("Unknown")
+//    }
+//    println(dayOfWeek)
 
-    val bg = 2.6
-    if(bg == 2.6){
-        println(true)
-    }
-    else if(bg == 2.5)
-        println("default")
-    else{
-        println(false)
-    }
 
-    //конструкция when
-    val isEnabled = true
-    when(isEnabled){
-        false -> println("isEnabledOFF")
-        true -> println("isEnabledON")
-    }
+    // Вызовы функций
+    sendMessage("Hello Kotlin")
+    sendMessage("Привет Kotlin")
+    sendMessage("Salut Kotlin")
 
-    val but = 30
-    when(but){
-        10 -> println("but = 10")
-        20 -> println("but = 20")
-        else -> println("неопределённое значение")
-    }
+    dispalyUser("Tom", 23)
+    dispalyUser("Alice", 19)
+    dispalyUser("Kate", 25)
 
-    when(but){
-        in 10..19 -> println("Значение в диапазоне от 10 до 19")
-        in 20 .. 29 -> println("Значение в диапазоне от 20 до 29")
-        !in 10..20 -> println("Значение вне диапазона 10..19")
-        else -> println("Неопределённое значение")
-    }
+    dispalyUserDefault("Tom", 23, "Manager")
+    dispalyUserDefault("Alice", 21)
+    dispalyUserDefault("Kate")
 
-    when(but){
-        10,20 -> println("Значение или 10 или 20")
-        else -> println("Неопределённое значение")
-    }
+    val arr:Array<Int> = arrayOf(1,2,3,4,5)
+    operationsArray(arr)
 
-    var ag = 10
-    when(ag){
-        10 -> {
-            println("ag = 10")
-            ag *= 2
-        }
-        20 -> {
-            println("ag = 20")
-            ag *= 5
-        }
-        else -> { println("неопределенное значение")}
-    }
-    println(ag)
+    printStrings("Tom", "Bob", "Sam")
+    printStrings("Kotlin", "JavaScript", "Java", "C#", "C++")
 
+    sum(1,2,3,4,5)
 
-    val a = 10
-    val b = 5
-    val c = 3
-    when(a){
-        b - c -> println("a = b - c")
-        b + 5 -> println("a = b + 5")
-        else -> println("неопределенное значение")
-    }
+    printUserGroup(3, "Tom", "Bob", "Alice")
+    val nums = intArrayOf(1,2,3,4)
+    changeNumber(*nums , koef = 2)
 
-    val cb = 14
-    val cc = 6
-    when{
-        (cb > 13) -> println("cb больше 13")
-        (cc > 13) -> println("cc больше 13")
-        else -> println("cb и cc меньше или равны 13")
-    }
+    hi()
+    hi()
+    hi()
 
-    val day = 2
-    var dayOfWeek = when(day){
-        1 -> "Monday"
-        2 -> "Tuesday"
-        3 -> "Wednesday"
-        4 -> "Thursday"
-        else -> ("Unknown")
-    }
-    println(dayOfWeek)
+    checkAge(18)
+    checkAge(101)
 
+    square(5)
     }
